57c61aae7d3f49f7d47ab786c1bf69a5d60ca654
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-11 22:14:15 +0600

    main layout set and create some components and set router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Main from './components/Layout/Main';
+import QuizTopics from './components/QuizTopics/QuizTopics';
 
 function App() {
-  const router = createBrowserRouter();
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/quizTopics',
+          element: <QuizTopics></QuizTopics>
+        }
+      ]
+    }
+  ]);
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
---
+++ src/components/QuizTopics/QuizTopics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuizTopics = () => {
+    return (
+        <div>
+            <h2>  Quiz Topics</h2>
+        </div>
+    );
+};
+
+export default QuizTopics;
