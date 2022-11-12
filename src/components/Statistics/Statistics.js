28580eabf91fd7a483fa31159b63c3ba83259ef5
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-12 16:07:41 +0600

    netlify.toml was added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -8,12 +8,12 @@ const Statistics = () => {
         <div className=' mx-0 lg:mx-auto w-full '>
             <h1 className='text-3xl font-bold text-violet-600 text-center my-12'> Statistics of all quiz topics</h1>
             <div className='flex justify-center my-5 '>
-                <BarChart width={500} height={300} data={quiz}>
+                <BarChart width={460} height={300} data={quiz}>
                     <XAxis dataKey="name" stroke="#8884d8" />
                     <YAxis />
                     <Tooltip />
                     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                    <Bar dataKey="total" fill="#8884d8" barSize={25} />
+                    <Bar dataKey="total" fill="#8884d8" barSize={20} />
                     <ResponsiveContainer />
                 </BarChart>
             </div>
