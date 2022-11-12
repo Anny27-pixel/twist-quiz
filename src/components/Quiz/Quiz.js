fbe3cb39e2b49c353db3a3dc7968640e53ff697e
Anny27-pixel <jabintasmin102523@gmail.com>
2022-11-12 13:12:04 +0600

    quiz component added and designed

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Question from '../Question/Question';
 
 const Quiz = () => {
     const quiz = useLoaderData().data;
@@ -8,6 +9,16 @@ const Quiz = () => {
     return (
         <div className='my-12 bg-violet-200 mx-28 rounded-2xl'>
             <h1 className='text-3xl font-bold text-center'>Quiz on : {name}</h1>
+            <h3 className='text-2xl font-semibold ml-12' >Number of Questions : {questions.length}</h3>
+            <div>
+                {
+                    questions.map(question => <Question
+                        key={question.id}
+                        question={question}
+
+                    ></Question>)
+                }
+            </div>
         </div>
     );
 };
